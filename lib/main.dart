a88f23326f396acbc7632f04f49c2ed0b02d5a0d
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-07-08 11:43:43 +0900

    centralize image in body

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("I am Rich")),
+      appBar: AppBar(
+        title: Text("I am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey[200],
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+        ),
+      ),
     ),
   ));
 }
