2693a9f4630a3712b6be4d3eb456c2197220b5ed
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-07-08 11:30:22 +0900

    Add app bar and add title

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+    home: Scaffold(
+      appBar: AppBar(title: Text("I am Rich")),
     ),
   ));
 }
