ce2f4b92b78b0b856ae337e7c1d766654b608e15
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-07-09 11:03:51 +0900

    email, phone container

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
           title: Text("I am Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
-        backgroundColor: Colors.blueGrey[200],
+        backgroundColor: Colors.lightGreen[300],
         body: SafeArea(
           child: Column(children: <Widget>[
             CircleAvatar(
@@ -28,16 +28,48 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                   fontWeight: FontWeight.w600),
             ),
-            Text(
-              "Flutter Developer",
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                color: Colors.white,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold
-              )
-            )
+            Text("Flutter Developer",
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold)),
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.green,
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "123 4567 8901",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20.0),
+                  )
+                ])),
+            Container(
+                color: Colors.green,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(children: <Widget>[
+                  Icon(Icons.email, color: Colors.white),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "shungo12tennis@gmail.com",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20.0),
+                  )
+                ]))
           ]),
         ),
       ),
